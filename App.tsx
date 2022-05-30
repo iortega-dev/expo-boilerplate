355dcbf2c8c084f46501353014e40fa86b8dd55a
iortega-dev <iortegamancera@gmail.com>
2022-05-30 22:04:08 +0200

    add basic intro component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	Box,
+	Center,
+	extendTheme,
+	Heading,
+	HStack,
+	Link,
+	NativeBaseProvider,
+	Text,
+	VStack,
+} from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const MyComponent = (): JSX.Element => (
+	<Center px={4} flex={1}>
+		<VStack space={5} alignItems="center">
+			<Heading size="lg">Welcome to NativeBase</Heading>
+			<HStack space={2} alignItems="center">
+				<Text>Edit</Text>
+				<Box
+					_web={{
+						_text: {
+							fontFamily: 'monospace',
+							fontSize: 'sm',
+						},
+					}}
+					px={2}
+					py={1}
+					bg="blueGray.200"
+				>
+					App.js
+				</Box>
+				<Text>and save to reload.</Text>
+			</HStack>
+			<Link href="https://docs.nativebase.io" isExternal>
+				<Text color="primary.500" underline fontSize={'xl'}>
+					Learn NativeBase
+				</Text>
+			</Link>
+		</VStack>
+	</Center>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Define the config
+const config = {
+	useSystemColorMode: false,
+	initialColorMode: 'light',
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
+const App = (): JSX.Element => {
+	return (
+		<NativeBaseProvider theme={theme}>
+			<MyComponent />
+		</NativeBaseProvider>
+	);
+};
+
+export default App;
